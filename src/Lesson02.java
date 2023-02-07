76b3b687b41fd606a045915fce6f4041a5605e6a
DianaHarmash <diana.v.garmash@gmail.com>
2023-02-07 16:30:27 +0200

    Комментарии ко второй домашке:)

--- src/Lesson02.java
+++ src/Lesson02.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class Lesson02 {
 
     public static void main(String[] args) {
-        /*
-       Task1
-        */
+        //** Task1 **//
         System.out.print("Enter random number: ");
         Scanner in = new Scanner(System.in);
         if (in.hasNextInt()) {
@@ -15,17 +13,13 @@ public class Lesson02 {
             System.out.println("Number doesn't exist ");
         }
 
-        /*
-       Task2
-        */
-        String name, surname;
-        int fullAge;
+        //** Task2 **//
         System.out.print("Enter your name: ");
-        name = in.next();
+        String name = in.next();
         System.out.print("Enter your surname: ");
-        surname = in.next();
+        String surname = in.next();
         System.out.print("Enter your full age: ");
-        fullAge = in.nextInt();
+        int fullAge = in.nextInt();
         System.out.println("Name: " + name + "\nSurname: " + surname + "\nFull age: " + fullAge);
     }
 }
